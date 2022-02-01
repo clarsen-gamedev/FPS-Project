9b6071634a95862d826ca2746a00a67eee33213a
Connor Larsen <clarsen.personal@gmail.com>
2022-02-01 14:39:11 -0500

    Create EnemyAI.cs

---
+++ Assets/Scripts/Enemy/AI/EnemyAI.cs
@@ -0,0 +1,24 @@
+// Name: EnemyAI.cs
+// Author: Connor Larsen
+// Date: 02/01/2022
+
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    // Serialized Variables
+
+    // Private Variables
+
+    // Grab the current state of the enemy AI
+    private string GetStateString()
+    {
+        string state = "NONE";  // Initialize "state" string as NONE;
+
+        // Check to see if CurrentState is NULL
+
+        // If and Else IF checks for all currently implemented states of the AI
+
+        return state;   // Return the current state
+    }
+}
