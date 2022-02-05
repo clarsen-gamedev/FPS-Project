966391f0f0ed3698741c0da916f53bb244848d9d
Connor Larsen <clarsen.personal@gmail.com>
2022-02-05 17:09:22 -0500

    Added Enemy Health Bars

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -3,18 +3,30 @@
 // Date: 02/01/2022
 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
-    // Public Variables
-    public float health = 100f;
+    // Serialized Variables
+    [SerializeField] Image healthBarImage;  // Reference to image used for enemy health UI
+    [SerializeField] float maxHealth = 100; // Maximum amount of health the enemy can have
+
+    // Private Variables
+    float currentHealth;    // Stores the current health of the enemy
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        currentHealth = maxHealth;  // Set currentHealth equal to maxHealth on script startup
+    }
 
     // Controls how the object attached takes damage
     public void TakeDamage(float damage)
     {
-        health -= damage;   // Subtract incoming damage from enemy health
+        currentHealth -= damage;                                // Subtract incoming damage from enemy health
+        healthBarImage.fillAmount = currentHealth / maxHealth;  // Calculate the percentage of fill amount for health UI
 
-        if (health <= 0f)   // Once the enemy is out of health
+        if (currentHealth <= 0f)    // Once the enemy is out of health
         {
             Die();  // Kill the enemy
         }
