f9c6914158139bd754075604621bc5d872f3e2f4
Connor Larsen <clarsen.personal@gmail.com>
2022-02-09 16:50:18 -0500

    Updates to Enemy State Machine

--- Assets/Scripts/Enemy/AI/EnemyIdleState.cs
+++ Assets/Scripts/Enemy/AI/EnemyIdleState.cs
@@ -10,19 +10,31 @@ public class EnemyIdleState : EnemyBaseState
     // Enter State Function
     public override void EnterState(EnemyStateMachine enemy)
     {
+        // DEBUG CODE
         Debug.Log("Enemy Entered 'Idle State'");                        // DEBUG: Print out a debug message
         enemy.GetComponent<Renderer>().material.color = Color.yellow;   // DEBUG: Make Idle enemies Yellow
+
+        // Reset bools
+        enemy.isWandering = false;
     }
 
     // Update State Function
     public override void UpdateState(EnemyStateMachine enemy)
     {
+        #region State Behaviour
+        // If the enemy isn't currently wandering...
+        if (enemy.isWandering == false)
+        {
+            
+        }
+        #endregion
+
         #region Transitions
         // Enemy Sees Player ==> Attack State
 
         // Enemy HP Reaches ~10% ==> Flee State
 
-        // Enemy HP Depleted ==> Dead State
+        // Dead State
         if (enemy.GetComponent<EnemyHealth>().GetEnemyHealth() <= 0)
         {
             enemy.SwitchState(enemy.deadState); // Switch to the dead state
